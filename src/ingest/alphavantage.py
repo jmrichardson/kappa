c91fa63a5cb659c69cc1cb0fa2c4c6f3af9042d3
John Richardson <jmrichardson@gmail.com>
2018-08-31 03:48:16 -0400

    Code patch

--- src/ingest/alphavantage.py
+++ src/ingest/alphavantage.py
@@ -1,11 +1,35 @@
 import logplus
 import plac
+import configparser
+import json
+from celery import Celery
 from alpha_vantage.timeseries import TimeSeries
 # from alpha_vantage.techindicators import TechIndicators
 # from alpha_vantage.sectorperformance import SectorPerformances
 # from alpha_vantage.cryptocurrencies import CryptoCurrencies
 
+# amqp://user:bitnami@192.168.1.100:5672//
+app = Celery('alphavantage', broker='amqp://user@localhost//')
 
-ts = TimeSeries(key='HRPMAL9U6F6FRB64')
-# Get json object with the intraday data and another with  the call's metadata
-data, meta_data = ts.get_daily_adjusted('MSFT')
+
+@app.task
+def add(x, y):
+    return x + y
+
+
+# @app.task
+def main(symbol: "Equity ticker symbol"):
+
+    config = configparser.ConfigParser()
+    config.read("../config.ini")
+    key = config.get("ingest.alphavantage", "key")
+
+    ts = TimeSeries(key=key)
+    # Get json object with the intraday data and another with  the call's metadata
+    data, meta_data = ts.get_daily_adjusted(symbol)
+
+    # print(json.dumps(data))
+
+
+if __name__ == '__main__':
+    plac.call(main)

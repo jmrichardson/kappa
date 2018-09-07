d66e49ec07361a693fa34839b8748cd9553bc400
John Richardson <jmrichardson@gmail.com>
2018-09-07 09:48:47 -0400

    General code update

---
+++ services/env.sh
@@ -0,0 +1,4 @@
+export node1='192.168.1.100'
+export node2='192.168.1.101'
+export whoami='node1'
+export zoo_cluster='false'
--- services/rabbitmq/cluster-entrypoint.sh
+++ services/rabbitmq/cluster-entrypoint.sh
@@ -1,23 +1,10 @@
 #!/bin/bash
-
 set -e
-
-# Start RMQ from entry point.
-# This will ensure that environment variables passed
-# will be honored
-/usr/local/bin/docker-entrypoint.sh rabbitmq-server -detached
-
-# Do the cluster dance
-rabbitmqctl stop_app
-rabbitmqctl join_cluster rabbit@rabbitmq1
-
-# Stop the entire RMQ server. This is done so that we
-# can attach to it again, but without the -detached flag
-# making it run in the forground
-rabbitmqctl stop
-
-# Wait a while for the app to really stop
-sleep 2s
-
-# Start it
+if [ `hostname` != "rabbitmq1" ]; then
+  /usr/local/bin/docker-entrypoint.sh rabbitmq-server -detached
+  rabbitmqctl stop_app
+  rabbitmqctl join_cluster rabbit@rabbitmq1
+  rabbitmqctl stop
+  sleep 5s
+fi
 rabbitmq-server
